1df4d7db7c02acb4b8f66f227129e8aab49c46c2
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-10-26 09:37:10 +0900

    controller index action 追加

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+      $item = Book:all();
+      return view('book.index')
+    }
 }
