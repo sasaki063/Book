98a998beccd3894e6aaef10cfe4ece36dfc93027
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-10-26 11:04:11 +0900

    view.store 修正

--- resources/views/book/store.blade.php
+++ resources/views/book/store.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 @section('content')
 
+@if (count($errors) > 0)
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+<table>
+  <form action="/book/store" method="post">
+  {{ csrf_field() }}
+    <tr>
+      <td>title:
+        <input type="text" name="title" value="{{old('title')}}">
+      </td>
+
+      <td>
+        text:<input type="text" name="text" value="{{old('text')}}">
+      </td>
+      <td>
+        <input type="submit" value="send">
+      </td>
+    </tr>
+  </form>
+</table>
 
 @endsection
