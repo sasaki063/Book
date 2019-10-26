0372415fe2eda39ae9435e4f12aa073c3b508d5f
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-10-26 09:39:42 +0900

    web.php 修正

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', function () {return view('welcome');});
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/book', 'BookController@index');
