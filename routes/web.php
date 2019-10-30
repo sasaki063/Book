0a65915dd53ca87943aa287ab1aa569ef4d3e452
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-10-30 17:02:33 +0900

    routes 修正

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,5 @@
 Route::get('/', function () {return view('welcome');});
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('book', 'BookController');
 
-// Route::prefix('book')->group(function(){
-//     Route::get('/', 'BookController@index');
-//     Route::get('store', 'BookController@store');
-//     Route::post('store', 'BookController@create');
-//     Route::get('edit', 'BookController@edit');
-//     Route::post('edit', 'BookController@update');
-// });
+Route::resource('book', 'BookController');
