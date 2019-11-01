83b0d08a18117bc98f5c7cbbc635493c466bb471
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-01 12:49:19 +0900

    view修正 DELETEボタン追加

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -10,8 +10,14 @@
        <tr>
         <td>{{ $item -> title }}</td>
         <td>{{ $item -> text }}</td>
+        <td>
+          <form action="{{ url('book/'.$item->id) }}" method="post">
+          {{ csrf_field() }}
+          {{ method_field('DELETE') }}
+          <button type="submit">削除</button>
+        </td>
        </tr>
       @endforeach
   </table>
-  
+
 @endsection
