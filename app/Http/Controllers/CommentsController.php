23f60ce6e5e8277c32f3dddfd630cbad5de91a3f
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-06 14:40:04 +0900

    comennts_controller store_action追加

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+      $params = $request->validate([
+        'book_id' => 'required|exists:books,id',
+        'body' => 'required|max:2000',
+      ]);
+
+      $book = Book::findOrFail($params['book_id']);
+
+      $comment = new Comment;
+      $comment->book_id = $request->book_id;
+      $comment->body = $request->body;
+      $comment->save();
+
+      return redirect()->route('book.show', ['book' => $book]);
+    }
 }
