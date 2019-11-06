390b5adb21c61934538b93abb49a285bbebacd6c
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-06 14:47:14 +0900

    Book.show View修正 コメントフォーム追加

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
+use App\Comment;
 
 class CommentsController extends Controller
 {
--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -7,4 +7,34 @@
     <li>{{ $book -> text }}</li>
   </ul>
 
+  <form method="POST" action="{{ route('comments.store') }}">
+    {{ csrf_field() }}
+
+    <input name="book_id" type="hidden" value="{{ $book->id }}">
+    <textarea id="body" name="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" rows="4">{{ old('body') }}</textarea>
+
+    @if ($errors->has('body'))
+    {{ $errors->first('body') }}
+    @endif
+
+    <button type="submit">コメントする</button>
+  </form>
+
+  @foreach ($book->comments as $item)
+  <table>
+    <tr>
+      <td>{{ $item -> id }}</td>
+      <td>{{ $item -> body }}</td>
+      <td>
+        <form action="{{ url('comments/'.$item->id) }}" method="post">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+        <button type="submit">削除</button>
+        </form>
+      </td>
+    </tr>
+  </table>
+  @endforeach
+
+
 @endsection
