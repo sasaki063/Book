9be10b05dcc5887f8ceb3132bb3c50c2cba9f060
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-06 14:36:16 +0900

    model修正 Book & Comment紐付け

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+  protected $fillable = [
+      'body',
+  ];
+
+  public function book()
+  {
+      return $this->belongsTo('App\Book');
+  }
 }
