7acd0d27e8f9752c4340c9c54d5ea36b328a6dec
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-08 14:41:07 +0900

    Home_Controller修正 indexアクション修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
+use App\User;
+use Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+      $user = Auth::id();
+      $query = Book::query();
+      $query->where('user_id',$user);
+      $items = $query->get();
+
+      $users = User::all();
+      return view('/home', ['items' => $items, 'users' => $users]);
     }
 }
