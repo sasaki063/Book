4abb4f66df35a8ce7ddd31c5c012ee8263fdc700
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-08 14:37:37 +0900

    User_model修正 books/commentsメソッド追加

--- app/User.php
+++ app/User.php
@@ -9,31 +9,24 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function books()
+    {
+      return $this->hasMany('App\Book');
+    }
+    public function comments()
+    {
+      return $this->hasMany('App\Comment');
+    }
 }
